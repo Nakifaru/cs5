dc510a90a3aadc7e4dde69283684916fe0b462b9
NaKifaru <michaelmwendwamumo@gmail.com>
2024-10-18 00:12:10 -0700

    Assembly code working

--- Hmmmwork6/hw6pr1c.py
+++ Hmmmwork6/hw6pr1c.py
@@ -7,12 +7,106 @@
 # You'll paste your 100 numbers in this triple-quoted string:
 #  (overwriting and removing these six numbers) 
 NUMBERS = """
+85
+88
+51
+74
+57
+0
 3
-42
+66
+89
+72
+15
+18
+81
+4
+87
+30
+33
+96
+19
+2
+45
+48
+11
+34
+17
+60
+63
+26
+49
+32
+75
+78
+41
+64
 47
-46
-91
+90
+93
+56
+79
+62
 5
+8
+71
+94
+77
+20
+23
+86
+9
+92
+35
+38
+1
+24
+7
+50
+53
+16
+39
+22
+65
+68
+31
+54
+37
+80
+83
+46
+69
+52
+95
+98
+61
+84
+67
+10
+13
+76
+99
+82
+25
+28
+91
+14
+97
+40
+43
+6
+29
+12
+55
+58
+21
+44
+27
+70
+73
+36
+59
+42
 """
 
 def unique(L):
@@ -24,7 +118,12 @@ def unique(L):
     elif ...            # check whether L[0] re-appears
     else ...            # otherwise...
     """
-    return False        # placeholder
+    if not L:
+        return True
+    elif L[0] in L[1:]:
+        return False 
+    else:
+        return unique(L[1:])
 
 
 def test(S):
